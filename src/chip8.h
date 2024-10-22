5510c8608f88a8ad53e0d63a8ae918711a744eca
Hans Buss <hans.buss@mailfence.com>
2024-10-21 22:09:11 -0300

    Add the initual structure and initializion phase of the chip 8 runtine.

---
+++ src/chip8.h
@@ -0,0 +1,8 @@
+#ifndef CHIP8_H
+	#define CHIP8_H 
+
+typedef struct chip8 Chip8;
+void init(Chip8* chip8);
+void emulate();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "chip8.h"
+
+int main(int argc, char *argv[])
+{
+	 Chip8 * chip8;
+	init(chip8);
+	return 0;
+}
